6f32cf25f97820bcea931a65397a184dab80cf84
Nuker0s <nuker0s2@gmail.com>
2023-11-07 21:06:38 +0100

    Update corridorgen.cs

--- Assets/corridorgen.cs
+++ Assets/corridorgen.cs
@@ -19,10 +19,12 @@ public class RoomGenerator : MonoBehaviour
     }
     public void roomgen()
     {
+        Transform wallroot;
+        float offset = size * quadsize;
         for (int i = 0; i < 4; i++)
         {
-            Transform wallroot = Instantiate(new GameObject("wallroot"),roomroot.position,quaternion.identity,roomroot).GetComponent<Transform>();
-            float offset = size * quadsize;
+            wallroot = Instantiate(new GameObject("wallroot"),roomroot.position,quaternion.identity,roomroot).GetComponent<Transform>();
+            
             wallroot.position += wallroot.right * offset*0.5f;
             for (int x = 0; x < size; x++)
             {
@@ -36,5 +38,41 @@ public class RoomGenerator : MonoBehaviour
             roomroot.Rotate(0, 90, 0);
         }
 
+        
+    }
+    public void kolanogen() 
+    {
+        float offset = size * quadsize;
+        Transform wallroot;
+        roomroot.Rotate(0, 90, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            wallroot = Instantiate(new GameObject("wallroot"), roomroot.position, quaternion.identity, roomroot).GetComponent<Transform>();
+            
+            wallroot.position += wallroot.right * offset * 0.5f;
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    Instantiate(quad, wallroot.position + new Vector3(x * quadsize, y * quadsize) - new Vector3(offset * 0.5f - quadsize / 2, offset * 0.5f - quadsize / 2), quaternion.identity, wallroot);
+
+                }
+            }
+            wallroot.Rotate(0, 90, 0);
+            roomroot.Rotate(0, 90, 0);
+        }
+        wallroot = Instantiate(new GameObject("wallroot"), roomroot.position, quaternion.identity, roomroot).GetComponent<Transform>();
+        
+        wallroot.position += wallroot.right * offset * 0.5f;
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                Instantiate(quad, wallroot.position + new Vector3(x * quadsize, y * quadsize) - new Vector3(offset * 0.5f - quadsize / 2, offset * 0.5f - quadsize / 2), quaternion.identity, wallroot);
+
+            }
+        }
+        wallroot.Rotate(0, 90, 0);
+        roomroot.Rotate(90, 0, 0);
     }
 }
