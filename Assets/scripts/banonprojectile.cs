c97e0bf85535e07852a9dfb0f43e7f013313abd6
Nuker0s <nuker0s2@gmail.com>
2023-11-26 18:36:17 +0100

    aa

--- Assets/scripts/banonprojectile.cs
+++ Assets/scripts/banonprojectile.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 [RequireComponent(typeof(Rigidbody))]
 public class banonprojectile : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed;
+    public float damage;
+    public float knockback;
+    public float range;
+    public bool explosive;
+    public VisualEffect onhit;
+    public VisualEffect trail;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed;
         Destroy(gameObject,10);
+        onhit.Stop();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.collider.gameObject.name);
+        
+        onhit.transform.parent = null;
+        trail.transform.parent = null;
+        onhit.Play();
+        Destroy(onhit.gameObject,10);
+        Destroy(trail.gameObject,10);
         Destroy(gameObject);
+
     }
 
 }
